44a8591f2e3f3605fc2ddbbc46119269a0eb7287
hinst <hinstance@yandex.ru>
2017-01-10 16:41:17 +0300

    1

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import sys
 
+def getLongestSequenceLength(text):
+    longestSequenceLength = 0
+    currentSequenceLength = 0
+    lastCharacter = ' '
+    for character in text:
+        if character != lastCharacter:
+            currentSequenceLength = 0
+        currentSequenceLength += 1
+        if longestSequenceLength < currentSequenceLength:
+            longestSequenceLength = currentSequenceLength
+        lastCharacter = character
+    return longestSequenceLength
+
 inputFile = sys.stdin
 if len(sys.argv) > 1:
     inputFile = open(sys.argv[1], 'r')
 lines = inputFile.readlines()
 size = int(lines[0].strip())
-print(size)
 for line in lines[1:]:
     line = line.strip()
-    print(line)
+
